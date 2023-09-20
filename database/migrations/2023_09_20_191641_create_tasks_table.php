60337bf918953fb9af24cf6b526437a8fe3ffc17
Diego C/G <diego.castilloguajardo@gmail.com>
2023-09-20 16:19:24 -0300

    feat: add task model and migrations

---
+++ database/migrations/2023_09_20_191641_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->timestamp('due_at')->nullable();
+            $table->boolean('status')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
