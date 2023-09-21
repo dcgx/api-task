13a6c5f9f7a669e52afe1b8d2176535aceb28213
Diego C/G <diego.castilloguajardo@gmail.com>
2023-09-20 23:40:29 -0300

    tests: update factory and seeder

--- database/migrations/2023_09_20_191641_create_tasks_table.php
+++ database/migrations/2023_09_20_191641_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->longText('description');
             $table->timestamp('due_at')->nullable();
             $table->boolean('status')->default(false);
             $table->softDeletes();
